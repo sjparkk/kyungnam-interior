3bd02c6aacd8639c63d72a930f18486e1a9bd56f
sjparkk <sjparkk0820@naver.com>
2021-11-22 22:05:41 +0900

    feat : TestMethodOrder 추가

--- src/test/java/com/kninterior/repository/BannerRepositoryTest.java
+++ src/test/java/com/kninterior/repository/BannerRepositoryTest.java
@@ -1,16 +1,21 @@
 package com.kninterior.repository;
 
 import com.kninterior.domain.Banner;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BannerRepositoryTest {
 
     @Autowired
     BannerRepository bannerRepository;
 
+    @Order(1)
     @Test
     public void create() {
         Banner banner = new Banner();
@@ -19,6 +24,11 @@ public class BannerRepositoryTest {
 
         Banner newBanner = bannerRepository.save(banner);
         System.out.println("newBanner : " + newBanner);
+    }
+
+    @Order(2)
+    @Test
+    public void read() {
     }
 
 }
