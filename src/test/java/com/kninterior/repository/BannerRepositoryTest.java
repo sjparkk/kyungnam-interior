f1db7a55879e6c95d09b0038553534f8ed238741
sjparkk <sjparkk0820@naver.com>
2021-11-22 22:02:11 +0900

    test : create test 추가

--- src/test/java/com/kninterior/repository/BannerRepositoryTest.java
+++ src/test/java/com/kninterior/repository/BannerRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.kninterior.repository;
 
+import com.kninterior.domain.Banner;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,4 +10,15 @@ public class BannerRepositoryTest {
 
     @Autowired
     BannerRepository bannerRepository;
+
+    @Test
+    public void create() {
+        Banner banner = new Banner();
+        banner.setImagePath("path.jpg");
+        banner.setViewOrder(1);
+
+        Banner newBanner = bannerRepository.save(banner);
+        System.out.println("newBanner : " + newBanner);
+    }
+
 }
