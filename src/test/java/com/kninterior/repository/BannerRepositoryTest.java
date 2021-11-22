ae2cc5d30b0dacded8704fa24cf423960be1f872
sjparkk <sjparkk0820@naver.com>
2021-11-22 21:59:24 +0900

    베너 리포지토리 테스트 추가

---
+++ src/test/java/com/kninterior/repository/BannerRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.kninterior.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BannerRepositoryTest {
+
+    @Autowired
+    BannerRepository bannerRepository;
+}
