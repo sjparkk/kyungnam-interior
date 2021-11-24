462d41a807ea61668fa7273ea1033e58e9daa9ca
sjparkk <sjparkk0820@naver.com>
2021-11-24 22:23:49 +0900

    test : ContactRepositoryTest create read 테스트 추가

---
+++ src/test/java/com/kninterior/repository/ContactRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.kninterior.repository;
+
+import com.kninterior.domain.Contact;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ContactRepositoryTest {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Test
+    @DisplayName("create & read 테스트")
+    public void Contact_저장하기() {
+        //given
+        Contact contact = Contact.builder()
+                .name("테스트")
+                .phone("01051711677")
+                .email("aaa@aaa.com")
+                .postCode("10323")
+                .address("경기도용인시수지구죽전로")
+                .addressDetail("도담단지")
+                .structureType("아파트")
+                .roomCnt(3)
+                .bathroomCnt(2)
+                .sizeNum(30)
+                .sizeType("평orm3")
+                .budget("1000만원")
+                .callTime("오후2시")
+                .password("1234")
+                .description("문의드립니다.")
+                .accessRoute("sns")
+                .build();
+        //when
+        contactRepository.save(contact);
+        //then
+        Assertions.assertEquals(contact, contactRepository.findById(1L).get());
+    }
+}
