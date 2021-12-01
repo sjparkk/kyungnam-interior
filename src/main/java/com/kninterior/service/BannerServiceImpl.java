b27d830537d093d88cd5b985d8ea93886e27ef6d
sjparkk <sjparkk0820@naver.com>
2021-12-01 20:54:59 +0900

    refactor : 라인 수정 및 메소드레퍼런스로 변경

--- src/main/java/com/kninterior/service/BannerServiceImpl.java
+++ src/main/java/com/kninterior/service/BannerServiceImpl.java
@@ -12,16 +12,12 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class BannerServiceImpl implements BannerService{
-
         private final BannerRepository bannerRepository;
-
 
         @Override
         public List<BannerDTO> getList() {
-
                 List<Banner> bannerList = bannerRepository.getBannerByViewOrder();
-
-                return bannerList.stream().map(banner -> entityToDTO(banner))
+                return bannerList.stream().map(this::entityToDTO)
                         .collect(Collectors.toList());
         }
 }
